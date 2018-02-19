84e4189e4c3fbc1fd9724442d862f059feae1bde
d-el <d2.718l@gmail.com>
2018-02-19 16:48:08 +0200

    Delete unuse function on clock.c

--- app/src/version.c
+++ app/src/version.c
@@ -8,10 +8,10 @@
 const uint32_t fwinfoMajor    = 1;
 const uint32_t fwinfoMinor    = 0;
 const uint32_t fwinfoRevision = 0;
-const uint32_t fwinfoBuild    = 514;
+const uint32_t fwinfoBuild    = 515;
 
-const char fwinfoVersion[] = {"1.0.0.514"};
+const char fwinfoVersion[] = {"1.0.0.515"};
 const char fwinfoDate[] = {"2018-02-19"};
-const char fwinfoTime[] = {"11:56:43"};
+const char fwinfoTime[] = {"14:47:04"};
 
 /***************** Copyright (C) Storozhenko Roman ******* END OF FILE *******/
