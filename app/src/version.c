348835dac347740a6623687984fb17eabf9bd176
d-el <d2.718l@gmail.com>
2018-03-08 20:35:06 +0200

    Minor edits

--- app/src/version.c
+++ app/src/version.c
@@ -8,10 +8,10 @@
 const uint32_t fwinfoMajor    = 1;
 const uint32_t fwinfoMinor    = 0;
 const uint32_t fwinfoRevision = 0;
-const uint32_t fwinfoBuild    = 558;
+const uint32_t fwinfoBuild    = 560;
 
-const char fwinfoVersion[] = {"1.0.0.558"};
-const char fwinfoDate[] = {"2018-02-23"};
-const char fwinfoTime[] = {"09:45:27"};
+const char fwinfoVersion[] = {"1.0.0.560"};
+const char fwinfoDate[] = {"2018-03-08"};
+const char fwinfoTime[] = {"18:34:22"};
 
 /***************** Copyright (C) Storozhenko Roman ******* END OF FILE *******/
