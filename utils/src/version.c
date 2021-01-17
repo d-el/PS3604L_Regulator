ea03162b1dfacaf4f86591189b229968d4869f88
d-el <d2.718l@gmail.com>
2021-01-17 22:02:56 +0200

    refactor
    Signed-off-by: d-el <d2.718l@gmail.com>

--- utils/src/version.c
+++ utils/src/version.c
@@ -3,13 +3,15 @@
 * @author	d_el
 */
 
+#include <stdint.h>
+
 const uint32_t fwinfoMajor    = 1;
 const uint32_t fwinfoMinor    = 3;
 const uint32_t fwinfoRevision = 0;
-const uint32_t fwinfoBuild    = 1313;
+const uint32_t fwinfoBuild    = 1315;
 
-const char fwinfoVersion[] = {"1.2.2.1313"};
-const char fwinfoDate[] = {"2021-01-05"};
-const char fwinfoTime[] = {"01:21:54"};
+const char fwinfoVersion[] = {"1.3.0.1315"};
+const char fwinfoDate[] = {"2021-01-17"};
+const char fwinfoTime[] = {"19:58:49"};
 
 /***************** Copyright (C) Storozhenko Roman ******* END OF FILE *******/
